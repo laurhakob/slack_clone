3e61f343fd27f99b5baa5f30a178cca7c26e1087
laurhakob <laurhakob@gmail.com>
2025-05-02 12:38:10 +0400

    thread functionality

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,19 +1,221 @@
+// "use client";
+
+// import {
+//   ResizableHandle,
+//   ResizablePanel,
+//   ResizablePanelGroup,
+// } from "@/components/ui/resizable";
+// import { Sidebar } from "./sidebar";
+// import { Toolbar } from "./toolbar";
+// import { WorkspaceSidebar } from "./workspace-sidebar";
+// import { usePanel } from "@/hooks/use-panel";
+
+// interface WorkspaceIdLayoutProps {
+//   children: React.ReactNode;
+// }
+
+// const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
+//   const { parentMessageId, onClose } = usePanel();
+
+//   const showPanel = !!parentMessageId;
+
+//   return (
+
+//       <div className="h-full">
+//         <Toolbar />
+//         <div className="flex h-[calc(100vh-40px)]">
+//           <Sidebar />
+//           <ResizablePanelGroup
+//             direction="horizontal"
+//             autoSaveId="ca-workspace-layout"
+//           >
+//             <ResizablePanel
+//               defaultSize={20}
+//               minSize={11}
+//               className="bg-[#5E2C5F]"
+//             >
+//               <WorkspaceSidebar />
+//             </ResizablePanel>
+//             <ResizableHandle withHandle />
+//             <ResizablePanel minSize={20}>{children}</ResizablePanel>
+//             {showPanel && (
+//               <>
+//                 <ResizableHandle withHandle />
+//                 <ResizablePanel minSize={20} defaultSize={29}>
+//                   Load thread
+//                 </ResizablePanel>
+//               </>
+//             )}
+//           </ResizablePanelGroup>
+//         </div>
+//       </div>
+//   );
+// };
+
+// export default WorkspaceIdLayout;
+
+// "use client";
+
+// import {
+//   ResizableHandle,
+//   ResizablePanel,
+//   ResizablePanelGroup,
+// } from "@/components/ui/resizable";
+// import { Sidebar } from "./sidebar";
+// import { Toolbar } from "./toolbar";
+// import { WorkspaceSidebar } from "./workspace-sidebar";
+// import { usePanel } from "@/hooks/use-panel";
+
+// interface WorkspaceIdLayoutProps {
+//   children: React.ReactNode;
+// }
+
+// const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
+//   const { parentMessageId, onClose } = usePanel();
+//   const showPanel = !!parentMessageId;
+
+//   return (
+//     <div className="h-full">
+//       <Toolbar />
+//       <div className="flex h-[calc(100vh-40px)]">
+//         <Sidebar />
+//         <ResizablePanelGroup direction="horizontal" autoSaveId="ca-workspace-layout">
+//           <ResizablePanel defaultSize={20} minSize={11} className="bg-[#5E2C5F]">
+//             <WorkspaceSidebar />
+//           </ResizablePanel>
+//           <ResizableHandle withHandle />
+//           <ResizablePanel minSize={20}>
+//             {children}
+//           </ResizablePanel>
+//           {showPanel && (
+//             <>
+//               <ResizableHandle withHandle />
+//               <ResizablePanel minSize={20} defaultSize={29}>
+//                 Load thread
+//               </ResizablePanel>
+//             </>
+//           )}
+//         </ResizablePanelGroup>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default WorkspaceIdLayout;
+
+
+
+
+// current 
+
+// "use client";
+
+// import {
+//   ResizableHandle,
+//   ResizablePanel,
+//   ResizablePanelGroup,
+// } from "@/components/ui/resizable";
+// import { Sidebar } from "./sidebar";
+// import { Toolbar } from "./toolbar";
+// import { WorkspaceSidebar } from "./workspace-sidebar";
+// import { usePanel } from "@/hooks/use-panel";
+// import { Loader } from "lucide-react";
+// import { Id } from "../../../../convex/_generated/dataModel";
+// import { Thread } from "@/features/messages/components/thread";
+
+// interface WorkspaceIdLayoutProps {
+//   children: React.ReactNode;
+// }
+
+// const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
+//   const { parentMessageId, onClose } = usePanel();
+//   const showPanel = !!parentMessageId;
+
+//   return (
+//     <div className="h-full">
+//       <Toolbar />
+//       <div className="flex h-[calc(100vh-40px)]">
+//         <Sidebar />
+//         <ResizablePanelGroup
+//           direction="horizontal"
+//           autoSaveId="ca-workspace-layout"
+//         >
+//           <ResizablePanel
+//             defaultSize={20}
+//             minSize={11}
+//             className="bg-[#5E2C5F]"
+//           >
+//             <WorkspaceSidebar />
+//           </ResizablePanel>
+//           <ResizableHandle withHandle />
+//           <ResizablePanel minSize={20}>{children}</ResizablePanel>
+//           {showPanel && (
+//             <>
+//               <ResizableHandle withHandle />
+//               <ResizablePanel minSize={20} defaultSize={29}>
+
+//               {parentMessageId ? (
+//                  <Thread
+//                     messageId={parentMessageId as Id<"messages">}
+//                     onClose={onClose}
+//                  />
+//                 )  : profileMemberId ? (
+//                   <Profile
+//                     memberId={profileMemberId as Id<"members">}
+//                     onClose={onClose}
+//                   />
+//                 ) : (
+//                   <div className="flex h-full items-center justify-center">
+//                     <Loader className="size-5 animate-spin text-muted-foreground" />
+//                   </div>
+//                 )}
+//               </ResizablePanel>
+//             </>
+//           )}
+//         </ResizablePanelGroup>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default WorkspaceIdLayout;
+
+
+
+
+
+
+
+// git hubic
+
 "use client";
+import { Loader } from "lucide-react";
+
+import { Thread } from "@/features/messages/components/thread";
+// import { Profile } from "@/features/members/components/profile";
 
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
+import { usePanel } from "@/hooks/use-panel";
+
 import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
 import { WorkspaceSidebar } from "./workspace-sidebar";
 
+import { Id } from "../../../../convex/_generated/dataModel";
+
 interface WorkspaceIdLayoutProps {
   children: React.ReactNode;
-}
+};
 
 const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
+  const { parentMessageId, profileMemberId, onClose } = usePanel();
+
+  const showPanel = !!parentMessageId || !!profileMemberId;
+
   return (
     <div className="h-full">
       <Toolbar />
@@ -28,10 +230,34 @@ const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
             minSize={11}
             className="bg-[#5E2C5F]"
           >
-            <WorkspaceSidebar  />
+            <WorkspaceSidebar />
           </ResizablePanel>
           <ResizableHandle withHandle />
-          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+          <ResizablePanel minSize={20} defaultSize={80}>
+            {children}
+          </ResizablePanel>
+          {showPanel && (
+            <>
+              <ResizableHandle withHandle />
+              <ResizablePanel minSize={20} defaultSize={29}>
+                {parentMessageId ? (
+                 <Thread
+                    messageId={parentMessageId as Id<"messages">}
+                    onClose={onClose}
+                 />
+                )  : profileMemberId ? (
+                  <Profile
+                    memberId={profileMemberId as Id<"members">}
+                    onClose={onClose}
+                  />
+                ) : (
+                  <div className="flex h-full items-center justify-center">
+                    <Loader className="size-5 animate-spin text-muted-foreground" />
+                  </div>
+                )}
+              </ResizablePanel>
+            </>
+          )}
         </ResizablePanelGroup>
       </div>
     </div>

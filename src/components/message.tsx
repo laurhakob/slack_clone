72196da2702df8353eb9f17666bd5c64199e79f3
laurhakob <laurhakob@gmail.com>
2025-05-02 17:00:19 +0400

    thread bar component

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -418,7 +418,7 @@ export const Message = ({
         <div
           className={cn(
             "flex flex-col gap-2 p-1.5 px-5 hover:bg-gray-100/60 group relative",
-            isEditing && "bg-[#f2c74433] hover:bg-[#f2c74433]",
+            isEditing && "bg-[#100f0f33] hover:bg-[#f2c74433]",
             isRemovingMessage &&
               "bg-rose-500/50 transform transition-all scale-y-0 origin-bottom duration-200"
           )}

ad1edc7aa429dc321cdfc5f4644a379d5af24332
laurhakob <laurhakob@gmail.com>
2025-05-03 01:38:26 +0400

    search dialog component

--- convex/channels.ts
+++ convex/channels.ts
@@ -30,8 +30,16 @@ export const remove = mutation({
       throw new Error("Unauthorized");
     }
 
+    const [messages] = await Promise.all([
+      ctx.db
+        .query("messages")
+        .withIndex("by_channel_id", (q) => q.eq("channelId", args.id))
+        .collect(),
+    ]);
 
-    // TODO: Remove associated messages
+    for (const message of messages) {
+      await ctx.db.delete(message._id);
+    }
 
     await ctx.db.delete(args.id);
 
